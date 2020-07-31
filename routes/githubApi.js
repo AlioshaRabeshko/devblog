53dcf63fd9987dc5c7c3e14b313efac6cfdee8fa
AlioshaRabeshko <Leflylll@gmail.com>
2020-07-31 15:22:25 +0300

    github api update once in 6min

--- routes/githubApi.js
+++ routes/githubApi.js
@@ -4,7 +4,9 @@ const express = require('express');
 const request = require('request');
 const router = express.Router();
 
-router.get('/', (req, response) => {
+let obj = null;
+
+setInterval(() => {
 	request(
 		{
 			url: 'https://api.github.com/users/aliosharabeshko',
@@ -13,6 +15,10 @@ router.get('/', (req, response) => {
 			},
 		},
 		(error, res) => {
+			if (error) {
+				console.log('error: ', error);
+				return response.status(404);
+			}
 			const {
 				login: userName,
 				avatar_url: image,
@@ -28,22 +34,33 @@ router.get('/', (req, response) => {
 					},
 				},
 				(error, res2) => {
-					const repos = JSON.parse(res2.toJSON().body).map((el) => {
-						const { name, html_url } = el;
-						return { name, repoUrl: html_url };
-					});
-					response.send({
-						userName,
-						image,
-						url,
-						followers,
-						following,
-						repos,
-					});
+					try {
+						const repos = JSON.parse(res2.toJSON().body).map((el) => {
+							const { name, html_url } = el;
+							return { name, repoUrl: html_url };
+						});
+						obj = {
+							userName,
+							image,
+							url,
+							followers,
+							following,
+							repos,
+						};
+					} catch (error) {
+						console.log('error: ', error);
+					}
 				}
 			);
 		}
 	);
+}, 360000);
+
+router.get('/', (req, response) => {
+	if (!obj) {
+		return response.status(404).send(null);
+	}
+	response.send(obj);
 });
 
 module.exports = router;
