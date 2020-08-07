d5d27a1ad8a245d60736234e6a37ddd1323d1d53
AlioshaRabeshko <Leflylll@gmail.com>
2020-08-07 11:43:31 +0300

    24s

--- routes/imagesApi.js
+++ routes/imagesApi.js
@@ -12,69 +12,67 @@ const imageminMozjpeg = require('imagemin-mozjpeg');
 const uniqueFilename = require('unique-filename');
 
 router.put('/link/:author', async (req, res) => {
-	request.get(
-		req.body.link,
-		{ encoding: null },
-		async (err, response, body) => {
-			if (err) return res.status(400).send(null);
-			const filename = `${uniqueFilename('./uploads')}.jpeg`;
-			const bufferMin = await imagemin.buffer(body, {
-				plugins: [
-					imageminMozjpeg({
-						quality: 75,
-					}),
-				],
-			});
-			const writeableStream = fs.createWriteStream(filename);
-			writeableStream.write(bufferMin);
-			writeableStream.end(() => {
-				const obj = {
-					name: filename.split('/')[1],
-					author: req.params.author,
-				};
-				Images.create(obj);
-				return res.json({ images: [obj], host: req.hostname });
-			});
-			writeableStream.on('error', (err) => console.log(err));
-		}
-	);
+	try {
+		request.get(
+			req.body.link,
+			{ encoding: null },
+			async (err, response, body) => {
+				if (err) return res.status(400).send({ err: 'Wrong link...' });
+				const filename = `${uniqueFilename('./uploads')}.jpeg`;
+				const bufferMin = await imagemin.buffer(body, {
+					plugins: [imageminMozjpeg({ quality: 75 })],
+				});
+				const writeableStream = fs.createWriteStream(filename);
+				writeableStream.write(bufferMin);
+				writeableStream.end(() => {
+					const obj = {
+						name: filename.split('/')[1],
+						author: req.params.author,
+					};
+					Images.create(obj);
+					return res.json({ images: [obj], host: req.hostname });
+				});
+				writeableStream.on('error', (err) => console.log(err));
+			}
+		);
+	} catch (err) {
+		return res.status(400).send({ msg: 'Something went wrong...' });
+	}
 });
 
 const upload = multer().array('files', 10);
 
 router.post('/upload/:author', upload, async (req, res, next) => {
-	const files = req.files;
-	const images = [];
-	if (!files) {
-		const error = new Error('No Files');
-		res.status(400).send({ error });
-		return next(error);
+	try {
+		const images = [];
+		if (!req.files) res.status(400).send({ msg: 'No files chosen...' });
+		for (const img of req.files) {
+			const filename = `${uniqueFilename('./uploads')}.jpeg`;
+			const buffer = await imagemin.buffer(img.buffer, {
+				plugins: [imageminMozjpeg({ quality: 75 })],
+			});
+			const writeableStream = fs.createWriteStream(filename);
+			writeableStream.write(buffer);
+			writeableStream.end();
+			writeableStream.on('error', (err) => console.log(err));
+			const obj = {
+				name: filename.split('/')[1],
+				author: req.params.author,
+			};
+			Images.create(obj);
+			images.push(obj);
+		}
+		return res.send({ images, host: req.hostname });
+	} catch (error) {
+		return res.status(400).send({ msg: 'Something went wrong...' });
 	}
-	for (const img of files) {
-		const filename = `${uniqueFilename('./uploads')}.jpeg`;
-		const buffer = await imagemin.buffer(img.buffer, {
-			plugins: [imageminMozjpeg({ quality: 75 })],
-		});
-		const writeableStream = fs.createWriteStream(filename);
-		writeableStream.write(buffer);
-		writeableStream.end();
-		writeableStream.on('error', function (err) {
-			console.log(err);
-		});
-		const obj = {
-			name: filename.split('/')[1],
-			author: req.params.author,
-		};
-		Images.create(obj);
-		images.push(obj);
-	}
-	return res.send({ images, host: req.hostname });
 });
 
 router.get('/:name', (req, res) => {
 	fs.exists(`uploads/${req.params.name}`, (exists) => {
-		if (exists)
-			res.sendFile(path.join(__dirname, `../uploads/${req.params.name}`));
+		return exists
+			? res.sendFile(path.join(__dirname, `../uploads/${req.params.name}`))
+			: res.status(404).send({ msg: 'There in no such image...' });
 	});
 });
 
